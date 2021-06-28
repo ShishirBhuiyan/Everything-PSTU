101bab02a67b0ef8b2cef7b490511670eb373dbf
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-06-28 17:21:51 +0600

    Header Slider

--- Asset/js/app.js
+++ Asset/js/app.js
@@ -1,10 +1,21 @@
 
 $(document).ready(function() {
-
     // Search Document
     $('.searchicon').click(function(){
         $(".search").toggleClass("open");
     }); 
 
+    // Banner Slick Slider
+    $('.content').slick({
+        dots: true,
+        infinite: true,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 7000,
+        prevArrow:'.prev',
+        nextArrow:'.next'
+        
+    });
 
 });
